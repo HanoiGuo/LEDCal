e41bb5687dddb6d6231bcae3b21280bff749307e
tony-dong <tony_dong@zhbojay.com>
2016-02-17 08:27:00 +0800

    master-2016-2-17

--- LED Calibration/AppDelegate.h
+++ LED Calibration/AppDelegate.h
@@ -9,7 +9,22 @@
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
+{
+    
+    IBOutlet NSTableView *tableView;
+    NSMutableArray *Namearray;
+    NSMutableArray *RightLEDarray;
+    NSMutableArray *LeftLEDarray;
+}
 
+@property (strong) IBOutlet NSTextField *Title;
+@property (strong) IBOutlet NSTextField *LeftROITitle;
+@property (strong) IBOutlet NSTextField *RightROITitle;
 
+@property (strong) IBOutlet NSImageView *LeftImageROI;
+@property (strong) IBOutlet NSImageView *RightImageROI;
+@property (strong) IBOutlet NSTextField *ResultLabel;
+
+- (IBAction)StartToCalibrate:(id)sender;
 @end
 
---
+++ LED Calibration/AppDelegate.mm
@@ -0,0 +1,62 @@
+//
+//  AppDelegate.m
+//  LED Calibration
+//
+//  Created by Hanoi on 16/2/16.
+//  Copyright (c) 2016年 Tony. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+    //设置部分textfield只读模式
+    _Title.editable = false;
+    _LeftROITitle.editable = false;
+    _RightROITitle.editable = false;
+    
+    
+    Namearray = [NSMutableArray arrayWithObjects:@"Best PWM",@"Final Brightness",@"Uniformity",nil];
+   [tableView reloadData];
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+//------------------------protocol----------------------------------
+
+//返回表格的行数
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView
+{
+    return [Namearray count];
+}
+//用了下面那个函数来显示数据就用不上这个，但是协议必须要实现，所以这里返回nil
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
+{
+    return nil;
+}
+//显示数据
+- (void)tableView:(NSTableView *)tableView willDisplayCell:(id)cell forTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
+{
+    NSString *data = [Namearray objectAtIndex:row];
+    NSString *identifier = [tableColumn identifier];
+    if ([identifier isEqualToString:@"name"])
+    {
+        NSTextFieldCell *textCell = cell;
+        [textCell setTitle: data];
+    }
+
+}
+- (IBAction)StartToCalibrate:(id)sender {
+}
+@end
